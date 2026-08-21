e9d6ab7a5cda98931e43714648cea391447c1cfb
bodhi <bodhi.brown@student.curtin.edu.au>
2026-08-21 16:33:53 +0800

    enhancement(16): Updated process noise inclusion in sensor filtering algs, now calculated from sensor measurement rate and possible variance per second. Updated functions accordingly, added function 'calculate_sensor_process_noise()'.

--- Integration_Board_Project/Core/Inc/sensor_filtering_algs.h
+++ Integration_Board_Project/Core/Inc/sensor_filtering_algs.h
@@ -17,11 +17,12 @@
 	} Sensor_Type;
 
 
-	/* Function declaration (actual function provided to filter sensor data) */
+	/* Function declarations */
 	void filter_sensor_data(int16_t data[3], Sensor_Type data_source);
+	void calculate_sensor_process_noise();
 
 	/* Testing functions (these functions are temporarily available to other files to test independently) */
-	int16_t predict_system_state_test(int16_t data, double* k, int16_t* e, int16_t* s, int16_t* p);
+	int16_t predict_system_state_test(int16_t data, double* k, int16_t* e, int16_t* s, int16_t p);
 	void filter_fixed_bias(int16_t raw_data[3], Sensor_Type data_source);
 	void kalman_state_estimation(int16_t data[3], Sensor_Type data_source);
 
--- Integration_Board_Project/Core/Src/sensor_filtering_algs.c
+++ Integration_Board_Project/Core/Src/sensor_filtering_algs.c
@@ -8,7 +8,6 @@
  * Filtering functions currently return arrays of filtered data, this approach is likely temporary, and is mainly for testing (allows for both the raw and filtered data to be compared)
 */
 
-
 /* include header files */
 #include "sensor_filtering_algs.h"
 #include <stdlib.h>
@@ -17,23 +16,32 @@
 /* Define Structs containing fixed bias for x+y+z axis on each sensor */
 /* define placeholder until all bias values can be determined */
 #define PLACEHOLDER_BIAS 0
-#define PLACEHOLDER_PROCESS_NOISE 100
+#define PLACEHOLDER_MEASUREMENT_VARIATION 1000
 
+/* VARIABLES FOR PROCESS NOISE CALCULATIONS */
+
+/* variables storing current data measurement rate in Hz for each sensor */
+static int16_t accel_measure_rate = 833;
+static int16_t gyro_measure_rate = 833;
+static int16_t mag_measure_rate = 100;
+
+/* variables storing estimated maximum measurement */
+static int16_t accel_measure_range = PLACEHOLDER_MEASUREMENT_VARIATION;
+static int16_t gyro_measure_range = PLACEHOLDER_MEASUREMENT_VARIATION;
+static int16_t mag_measure_range = PLACEHOLDER_MEASUREMENT_VARIATION;
+
+/* STRUCTS/VARIABLES FOR FIXED/STATE ESTIMATION FILTERING*/
 typedef struct {
 	int16_t x;
 	int16_t y;
 	int16_t z;
 } Fixed_Bias;
 
-
-/* UNSURE if this will be final implementation, may hopefully figure out how to optomise in future (i.e. requires less struct instances) */
-
 /* Struct used to maintain variables required for state prediction algorithm */
 typedef struct {
 	double kalman_gain;
 	int16_t estimation_variation;
 	int16_t state_estimation;
-	int16_t process_noise;
 } State_Prediction_Variables;
 
 /* Struct used to store variables required to filter each reading from IMU && MAG */
@@ -41,6 +49,7 @@ typedef struct {
 	State_Prediction_Variables x;
 	State_Prediction_Variables y;
 	State_Prediction_Variables z;
+	int16_t sensor_process_noise;
 } Sensor_Reading_Filtering;
 
 /* Will update each struct instance to contain appropriate value when able */
@@ -49,9 +58,22 @@ const static Fixed_Bias gyro_fixed_bias = {PLACEHOLDER_BIAS, PLACEHOLDER_BIAS, P
 const static Fixed_Bias mag_fixed_bias = {PLACEHOLDER_BIAS, PLACEHOLDER_BIAS, PLACEHOLDER_BIAS};
 
 /* Store variables required to complete state prediction filtering algorithms for each sensor measurement */
-static Sensor_Reading_Filtering accel_filtered_state = { {0.0, 0, 0, PLACEHOLDER_PROCESS_NOISE}, {0.0, 0, 0, PLACEHOLDER_PROCESS_NOISE}, {0.0, 0, 0, PLACEHOLDER_PROCESS_NOISE} };
-static Sensor_Reading_Filtering gyro_filtered_state = { {0.0, 0, 0, PLACEHOLDER_PROCESS_NOISE}, {0.0, 0, 0, PLACEHOLDER_PROCESS_NOISE}, {0.0, 0, 0, PLACEHOLDER_PROCESS_NOISE} };
-static Sensor_Reading_Filtering mag_filtered_state = { {0.0, 0, 0, PLACEHOLDER_PROCESS_NOISE}, {0.0, 0, 0, PLACEHOLDER_PROCESS_NOISE}, {0.0, 0, 0, PLACEHOLDER_PROCESS_NOISE} };
+/* '-1' is subsituted for process noise, process noise must be calculated before using these structs via calculate_sensor_process_noise() function */
+static Sensor_Reading_Filtering accel_filtered_state = { {0.0, 0, 0}, {0.0, 0, 0}, {0.0, 0, 0}, -1 };
+static Sensor_Reading_Filtering gyro_filtered_state = { {0.0, 0, 0}, {0.0, 0, 0}, {0.0, 0, 0}, -1 };
+static Sensor_Reading_Filtering mag_filtered_state = { {0.0, 0, 0}, {0.0, 0, 0}, {0.0, 0, 0}, -1 };
+
+/*
+* Function to calculate process noise for each sensor (updates sensor_process_noise in required structs)
+* Takes no params, no return value
+* Must be called at least once before data is to be read from sensors
+*/
+void calculate_sensor_process_noise() {
+	/* May update function to also update measure_rate for each sensor (must read from each sensor's registers, prolly unnecessary aswell) */
+	accel_filtered_state.sensor_process_noise = accel_measure_range / (int16_t)( 1.0 / (double)accel_measure_rate );
+	gyro_filtered_state.sensor_process_noise = gyro_measure_range / (int16_t)( 1.0 / (double)gyro_measure_rate );
+	mag_filtered_state.sensor_process_noise = mag_measure_range / (int16_t)( 1.0 / (double)mag_measure_rate );
+}
 
 /*
 * Function to filter fixed bias from raw sensor readings
@@ -118,11 +140,11 @@ static int16_t calculate_state_estimation(int16_t x, double k, int16_t z) {
 
 /*
 * Function used to combine kalman state estimation filtering algorithms into single filtering operation for a single data reading
-* imports data (new measurement to be filtered), state_predict_vars (reference to struct containing appropriate variables to apply state prediction)
+* imports data (new measurement to be filtered), state_predict_vars (reference to struct containing appropriate variables to apply state prediction), process_noise (process noise of sensor)
 * Updates values stored within state_predict_vars to reflect new state prediction variables (state_predict_vars->state_estimation is filtered data)
 * Exports: value of state_predict_vars->state_estimation after prediction occurs
 */
-int16_t predict_system_state(int16_t data, State_Prediction_Variables* state_predict_vars) {
+int16_t predict_system_state(int16_t data, State_Prediction_Variables* state_predict_vars, int16_t process_noise) {
 	int16_t measurement_variance;
 	
 	// calculate variance in current measurement from estimated state
@@ -130,7 +152,7 @@ int16_t predict_system_state(int16_t data, State_Prediction_Variables* state_pre
 
 	//apply state estimation algorithms in order (kalman->estimate_variation->state_estimation)
 	state_predict_vars->kalman_gain = calculate_kalman_gain(state_predict_vars->estimation_variation, measurement_variance);
-	state_predict_vars->estimation_variation = calculate_estimate_variation(state_predict_vars->kalman_gain, state_predict_vars->estimation_variation, state_predict_vars->process_noise);
+	state_predict_vars->estimation_variation = calculate_estimate_variation(state_predict_vars->kalman_gain, state_predict_vars->estimation_variation, process_noise);
 	state_predict_vars->state_estimation = calculate_state_estimation(state_predict_vars->state_estimation, state_predict_vars->kalman_gain, data);
 
 	return state_predict_vars->state_estimation;
@@ -141,13 +163,13 @@ int16_t predict_system_state(int16_t data, State_Prediction_Variables* state_pre
 * Imports data (new measurement), k (kalman gain), e (estimation variation), s (state estimation), p (process noise)
 * Exports new data (after filtering applied)
 */
-int16_t predict_system_state_test(int16_t data, double* k, int16_t* e, int16_t* s, int16_t *p) {
+int16_t predict_system_state_test(int16_t data, double* k, int16_t* e, int16_t* s, int16_t p) {
 	// This function is only to temporarily exist to allow external files to test exlsuively the kalman filtering function (without fixed bias removal
 	// Function logic mirrors predict_system_state, using imported values as opposed to struct
 	int16_t r = abs(data - *(s));
 
 	*k = calculate_kalman_gain(*e, r);
-	*e = calculate_estimate_variation(*k, *e, *p);
+	*e = calculate_estimate_variation(*k, *e, p);
 	*s = calculate_state_estimation(*s, *k, data);
 
 	// explicity return filtered value, other values updated via address 
@@ -165,19 +187,19 @@ void kalman_state_estimation(int16_t data[3], Sensor_Type data_source) {
 	//filter data using appropriate state-estimation variables determined on source of data
 	switch(data_source) {
 		case ACCELEROMETER:
-			data[0] = predict_system_state(data[0], &(accel_filtered_state.x)); // x
-			data[1] = predict_system_state(data[1], &(accel_filtered_state.y)); // y
-			data[2] = predict_system_state(data[2], &(accel_filtered_state.z)); // z
+			data[0] = predict_system_state(data[0], &(accel_filtered_state.x), accel_filtered_state.sensor_process_noise); // x
+			data[1] = predict_system_state(data[1], &(accel_filtered_state.y), accel_filtered_state.sensor_process_noise); // y
+			data[2] = predict_system_state(data[2], &(accel_filtered_state.z), accel_filtered_state.sensor_process_noise); // z
 			break;
 		case GYROSCOPE:
-			data[0] = predict_system_state(data[0], &(gyro_filtered_state.x)); // x
-			data[1] = predict_system_state(data[1], &(gyro_filtered_state.y)); // y
-			data[2] = predict_system_state(data[2], &(gyro_filtered_state.z)); // z
+			data[0] = predict_system_state(data[0], &(gyro_filtered_state.x), gyro_filtered_state.sensor_process_noise); // x
+			data[1] = predict_system_state(data[1], &(gyro_filtered_state.y), gyro_filtered_state.sensor_process_noise); // y
+			data[2] = predict_system_state(data[2], &(gyro_filtered_state.z), gyro_filtered_state.sensor_process_noise); // z
 			break;
 		case MAGNETOMETER:
-			data[0] = predict_system_state(data[0], &(mag_filtered_state.x)); // x
-			data[1] = predict_system_state(data[1], &(mag_filtered_state.y)); // y
-			data[2] = predict_system_state(data[2], &(mag_filtered_state.z)); // z
+			data[0] = predict_system_state(data[0], &(mag_filtered_state.x), mag_filtered_state.sensor_process_noise); // x
+			data[1] = predict_system_state(data[1], &(mag_filtered_state.y), mag_filtered_state.sensor_process_noise); // y
+			data[2] = predict_system_state(data[2], &(mag_filtered_state.z), mag_filtered_state.sensor_process_noise); // z
 			break;
 	}
 }
